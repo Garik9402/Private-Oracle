40fb21dc722beae7381b3bee6effeebeb5be2bf6
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-04-26 22:56:20 +0300

    add;)

--- js/static.js
+++ js/static.js
@@ -1,19 +1,21 @@
 //search
-let searchNode = document.querySelector('.header-searchlogin__icon, .inputtext')
-let inputNode = document.querySelector('.inputtext')
-
+let searchNode = document.querySelector('.searchlogin__icon,inputtext')
+let inputNode = document.querySelectorAll('.inputtext')
 window.addEventListener('click', function(e) {
     if (searchNode.contains(e.target) || inputNode.contains(e.target)) {
-        inputNode.classList.add('inputactive')
+        inputNode.classList.add('inputtext--inputactive')
+
     } else {
-        inputNode.classList.remove('inputactive')
+        inputNode.classList.remove('inputtext--inputactive')
     }
 });
 
 //burger
 let burgerNode = document.querySelector('.burger')
-let mainNode = document.querySelector('.header-top__continerswrap')
+let mainNode = document.querySelector('.m-main__container')
+let contentNode = document.querySelector('.wrapper-content')
 burgerNode.addEventListener('click', function() {
-    burgerNode.classList.toggle('active')
-    mainNode.classList.toggle('active2')
+    burgerNode.classList.toggle('burger--active')
+    mainNode.classList.toggle("m-main__container--open")
+    contentNode.classList.toggle('wrapper-content--close')
 })

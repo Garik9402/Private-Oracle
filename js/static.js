7897eb3250aa6745f541c11c4dad6fff76d2b177
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-04-27 15:46:44 +0300

    add ;)

--- js/static.js
+++ js/static.js
@@ -1,6 +1,6 @@
 //search
-let searchNode = document.querySelector('.searchlogin__icon,inputtext')
-let inputNode = document.querySelectorAll('.inputtext')
+let searchNode = document.querySelector('.m-main__searchlogin-icon ')
+let inputNode = document.querySelector('.m-main__inputtext')
 window.addEventListener('click', function(e) {
     if (searchNode.contains(e.target) || inputNode.contains(e.target)) {
         inputNode.classList.add('inputtext--inputactive')
@@ -10,6 +10,16 @@ window.addEventListener('click', function(e) {
     }
 });
 
+let headerSearchNode = document.querySelector('.header__searchlogin-icon')
+let headerinputNode = document.querySelector('.header__inputtext')
+window.addEventListener('click', function(e) {
+    if (headerSearchNode.contains(e.target) || headerinputNode.contains(e.target)) {
+        headerinputNode.classList.add('inputtext--inputactive')
+
+    } else {
+        headerinputNode.classList.remove('inputtext--inputactive')
+    }
+});
 //burger
 let burgerNode = document.querySelector('.burger')
 let mainNode = document.querySelector('.m-main__container')

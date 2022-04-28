054418e8a9bcd671cbde4a0d1a2c6a44278d392b
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-04-28 10:56:34 +0300

    add:)

--- js/static.js
+++ js/static.js
@@ -1,10 +1,27 @@
+// undrline link
+let mainlinkNode = document.querySelectorAll('.main__link')
+for (i = 0; i < mainlinkNode.length; i++) {
+    mainlinkNode[i].addEventListener('mouseover', function() {
+        for (let d = 0; d < mainlinkNode.length; d++) {
+            mainlinkNode[d].classList.remove('main__link--underline-active')
+        }
+        this.classList.add('main__link--underline-active')
+    })
+    for (let j = 0; j < mainlinkNode.length; j++) {
+        mainlinkNode[j].addEventListener('mouseout', function() {
+            this.classList.remove('main__link--underline-active')
+
+
+        })
+    }
+}
+
 //search
 let searchNode = document.querySelector('.m-main__searchlogin-icon ')
 let inputNode = document.querySelector('.m-main__inputtext')
 window.addEventListener('click', function(e) {
     if (searchNode.contains(e.target) || inputNode.contains(e.target)) {
         inputNode.classList.add('inputtext--inputactive')
-
     } else {
         inputNode.classList.remove('inputtext--inputactive')
     }
@@ -15,7 +32,6 @@ let headerinputNode = document.querySelector('.header__inputtext')
 window.addEventListener('click', function(e) {
     if (headerSearchNode.contains(e.target) || headerinputNode.contains(e.target)) {
         headerinputNode.classList.add('inputtext--inputactive')
-
     } else {
         headerinputNode.classList.remove('inputtext--inputactive')
     }
@@ -27,5 +43,5 @@ let contentNode = document.querySelector('.wrapper-content')
 burgerNode.addEventListener('click', function() {
     burgerNode.classList.toggle('burger--active')
     mainNode.classList.toggle("m-main__container--open")
-    contentNode.classList.toggle('wrapper-content--close')
+    contentNode.classList.toggle('wrapper-content--toggle')
 })
